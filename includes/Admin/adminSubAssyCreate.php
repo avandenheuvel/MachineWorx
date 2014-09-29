c59ea0f6fd1ea00f2135ad8799dd57f3655b7301
apvandenheuvel <apvandenheuvel@yahoo.com>
2014-09-29 13:41:21 -0500

    Some modifications to sub assy create form

--- includes/Admin/adminSubAssyCreate.php
+++ includes/Admin/adminSubAssyCreate.php
@@ -1,7 +1,11 @@
 <h2>Create Sub-Assembly</h2>
 
 <div id="editContainer">
+	<p>Set up sub assembly by picking items from the available items section and moving them
+		to the selected items area. If the check you require is not available it can be made on the
+		<a href="#" id="link"onclick="setUpOptions('Create Item')">Create Items</a> page.</p>
 	<div id="column1">
+	<h3>Available items</h3>	
 	<select multiple size=12 id="select1">
 		<script>//Define options for building list of available checks to edit
 		
@@ -57,6 +61,7 @@
 		</div>
 		
 		<div id="column2right">
+			<h3>Selected Items:</h3>
 			<select size=12 id="select2"></select>
 			<input type=button class="button" id="move up" onclick="changeOrder()" name="moveUp" value="Move Up" />
 			<input type=button class="button" id="move down" onclick="changeOrder()" name="moveDown" value="Move Down" />
