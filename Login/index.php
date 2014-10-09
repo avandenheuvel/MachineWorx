e36f48055a191646c9c4e55fdf4662b50136fbb0
austinlem <austin.lem@gmail.com>
2014-10-08 19:53:13 -0500

    Frameworking/Testing

---
+++ Login/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login to MachineWorx</title>
+<link rel="stylesheet" type="text/css" href="../index.css"/>
+</head>
+
+<body>
+<input type="text"/>
+</body>
+</html>
