522168482faca893b0f13eb541d196f99a7441fd
apvandenheuvel <apvandenheuvel@yahoo.com>
2014-10-30 11:11:43 -0500

    added function to push selected data to the description area

--- Admin/adminItemEdit.php
+++ Admin/adminItemEdit.php
@@ -1,6 +1,14 @@
+<script>
+	function populateDesc(In){
+		$('#checkDesc').val(In.value);
+	}
+</script>
 
 <?php
-	
+	/**
+	 * Connect to the databese and retrieve available items
+	 */
+
 	$dsn = 'mysql:host=itsql.fvtc.edu;dbname=machineworx158';
 	$username = 'MachineWorx158';
 	$ServerPassword='MachineWorx158';
@@ -11,7 +19,7 @@
 		$db = new PDO($dsn,$username,$ServerPassword,$options);
 			
 		
-		$SQL = $db->prepare("Select * from tblChecks");
+		$SQL = $db->prepare("Select Check_Name from tblChecks");
 		$SQL->execute();//execute the SQL query
 		$Check = $SQL->fetch();//Gets the first row of the table
 		
@@ -21,15 +29,13 @@
 			//Gets the current row value from the appropriate column
 			//$CheckID = $Check['CheckID'];
 			$CheckName = $Check['Check_Name'];
-			$CheckDesc = $Check['Check_Desc'];
+			//$CheckDesc = $Check['Check_Desc'];
 			//$CheckType=$Check['Check_Type'];
 			
 			$arr[] = array('Check_Name' => $CheckName/*, 'Check_Desc' => $CheckDesc*/);
 			
 			$Check = $SQL->fetch();//fetch the next row*/
 		}
-
-		//echo json_encode($arr);
 		
 		$SQL->closeCursor(); //disconnect from the server
 		$db = null; // Clear memory
@@ -40,10 +46,6 @@
 	}
 ?>
 
-<script>
-	var checkNamesArray=<?=json_encode($arr)?>;
-</script>
-
 <form>
 	<div id="editContainer">
 		<h2>Edit Item check</h2>
@@ -51,14 +53,13 @@
 			If the item check you require is not available it can be made on the
 			<a href class="link"onclick='setUpAdmin("Create Item"); return false;'>Create Items</a> page.</p><!--Calls function in adminOptions.php-->
 
-		<h4>Select Check</h4><select class="dropdown" id="available">
-			<script>//Define options for building list of available checks to edit
+		<h4>Select Check</h4><select class="dropdown" id="available" onchange="populateDesc(this);">
+			<script>
+				//Define options for building list of available checks to edit
 				
-				//Simple array for now. Will require a PHP db query
-				/*var Options = ["Check 1", "Check 2", "Check 3",
-				"Check 4", "Check 5", "Check 6", "Check...."];*/
+				var checkNamesArray=<?=json_encode($arr)?>;
 				
-				
+				//Build the list of available items
 				for(var i = 0; i<checkNamesArray.length; i++ ){
 					var link = document.createElement('option');
 					link.className = "";
@@ -66,7 +67,7 @@
 					link.href = '#';
 					//link.style.width="100%";
 					//link.style.margin="1px";
-					//link.onclick = function(){setUpAdmin(this)};
+					link.onclick = function(){populateDesc(this);};
 					document.getElementById('available').appendChild(link);
 				}
 			</script>
@@ -76,7 +77,7 @@
 	
 		<!--Area for user to select which item to edit-->
 		<label for="txtInput">Check Description: </label>
-		<textarea rows="5" cols="50" class="txtInput"></textarea>	
+		<textarea id="checkDesc" rows="5" cols="50" class="txtInput"></textarea>	
 		    
 	   </br>
 	    <!--Select the type of item-->
